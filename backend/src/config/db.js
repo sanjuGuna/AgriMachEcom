d0798e7f2ee17656181bd636f3a6a745d3b590ef
sanjuGuna <sanjay27it@gmail.com>
2026-01-22 17:04:33 +0530

    Remove folder from repository

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database connected successfully");
+    }catch(error){
+        console.log("Error occured while connecting",error);
+        process.exit(1);
+    }
+}
+
+module.exports=connectDB;
---
+++ backend/src/index.js
@@ -0,0 +1,3 @@
+require('dotenv').config();
+const connectDB=require("./config/db");
+connectDB();
