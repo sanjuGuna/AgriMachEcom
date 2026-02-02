c502c8c1a60bd93dbd46e513aff24af7c6f524ee
sanjuGuna <sanjay27it@gmail.com>
2026-02-02 21:49:31 +0530

    removed loging for privateKeys

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,10 +1,4 @@
 const cloudinary = require("cloudinary").v2;
-console.log("Cloudinary ENV:", {
-  name: process.env.CLOUDINARY_CLOUD_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secret: process.env.CLOUDINARY_API_SECRET ? "SET" : "MISSING"
-});
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
